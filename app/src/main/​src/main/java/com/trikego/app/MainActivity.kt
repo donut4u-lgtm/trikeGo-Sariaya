989d935eedee102241ca6fd343fd2ff20a4df3eb
donut4u-lgtm <donut4u@gmail.com>
2026-09-09 19:47:52 +0800

    Update MainActivity.kt

--- app/src/main/​src/main/java/com/trikego/app/MainActivity.kt
+++ app/src/main/​src/main/java/com/trikego/app/MainActivity.kt
@@ -3,56 +3,24 @@ package com.trikego.app
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        val layout = LinearLayout(this)
-        layout.orientation = LinearLayout.VERTICAL
-        layout.setPadding(40, 100, 40, 40)
-
-        val passenger = Button(this)
-        passenger.text = "PASSENGER"
-
-        passenger.setOnClickListener {
-            Toast.makeText(
-                this,
-                "PASSENGER BUTTON WORKS",
-                Toast.LENGTH_LONG
-            ).show()
+        findViewById<Button>(R.id.btnPassenger).setOnClickListener {
+            Toast.makeText(this, "Passenger Mode Clicked!", Toast.LENGTH_SHORT).show()
         }
 
-        val driver = Button(this)
-        driver.text = "DRIVER"
-
-        driver.setOnClickListener {
-            Toast.makeText(
-                this,
-                "DRIVER BUTTON WORKS",
-                Toast.LENGTH_LONG
-            ).show()
+        findViewById<Button>(R.id.btnDriver).setOnClickListener {
+            Toast.makeText(this, "Driver Mode Clicked!", Toast.LENGTH_SHORT).show()
         }
 
-        val admin = Button(this)
-        admin.text = "ADMIN"
-
-        admin.setOnClickListener {
-            Toast.makeText(
-                this,
-                "ADMIN BUTTON WORKS",
-                Toast.LENGTH_LONG
-            ).show()
+        findViewById<Button>(R.id.btnAdmin).setOnClickListener {
+            Toast.makeText(this, "Admin Mode Clicked!", Toast.LENGTH_SHORT).show()
         }
-
-        layout.addView(passenger)
-        layout.addView(driver)
-        layout.addView(admin)
-
-        setContentView(layout)
     }
 }
