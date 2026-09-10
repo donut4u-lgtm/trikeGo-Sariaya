5e3a0ca7d85eb2d3cf9069edf571ad0e1fe3ceeb
donut4u-lgtm <donut4u@gmail.com>
2026-09-10 16:21:09 +0800

    Update MainActivity.kt

--- app/src/main/​src/main/java/com/trikego/app/MainActivity.kt
+++ app/src/main/​src/main/java/com/trikego/app/MainActivity.kt
@@ -1,11 +1,1 @@
 
-package com.trikego.app
-
-import android.app.Activity
-import android.os.Bundle
-
-class MainActivity : Activity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-}
